816e5439565dfa813adcb12f1c3ea1326fbbfb7c
DivyaraniPhondekar <37135013+DivyaraniPhondekar@users.noreply.github.com>
2018-04-04 12:53:26 +0530

    Update TomcatImpl.sh

--- Shell Script/Assign3/TomcatImpl.sh
+++ Shell Script/Assign3/TomcatImpl.sh
@@ -4,9 +4,9 @@
 sudo yum install tomcat7
 
 # To start Tomcat:
-sudo service tomcat6 start
+sudo service tomcat7 start
 
 # To stop Tomcat:
-sudo service tomcat6 stop
+sudo service tomcat7 stop
 
  sudo /sbin/iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
